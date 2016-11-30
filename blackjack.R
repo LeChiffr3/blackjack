8f62b7df83820a31a7d2c9bf69eebf07b5fadf4d
LeChiffr3 <matthieu.pommier@gmail.com>
2016-11-30 12:22:52 +0100

    Update blackjack.R

--- blackjack.R
+++ blackjack.R
@@ -1,15 +1,13 @@
 library(dplyr)
+llibrary(ggplot)
 
 decks_nb <- 6
 
 #The dealer hand
 
 dealer <- function() {
- 
   deck <- tibble(card = as.integer(1:10), count = decks_nb*as.integer(c(rep(4, 9), 16))) #The 6 decks
- 
   first <- numeric(0) # The first car drew by the dealer
- 
   values <- 0 #the value(s) of the dealer's hand
  
   while(all(values < 17)  |  (any(values < 17) & any(values > 21))){    #while all the values are < 17 or if there is one above but it's already bust
@@ -18,8 +16,7 @@ dealer <- function() {
    
     first <- ifelse(length(first) == 0, draw, first) # keep the first draw
     values <- c(values + draw, values + if(draw == 1) 11) # Add the draw to the value of the hand, you double the number of values if it's an Ace
-   
-  }
+     }
  
   result <- values[values <= 21] #Keep only values under 21
   result <- if(length(result) == 0) min(values) else result #Except if there is none
